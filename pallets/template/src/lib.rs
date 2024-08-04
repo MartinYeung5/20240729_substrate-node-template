f0472e4f173365f5365026b25232d1e425b71a71
MartinYeung5 <luckylucky2010201@yahoo.com.hk>
2024-08-04 14:18:10 +0800

    20240804build21

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -7,9 +7,9 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+  #[pallet::pallet]
+  #[pallet::generate_store(pub(super) trait Store)]
+  pub struct Pallet<T>(_);
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
